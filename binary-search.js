a26663ee438af7eb5053ad53fe4608b834803151
TechhDan <tech.dan2689@gmail.com>
2022-08-12 10:04:50 -0400

    Recursive binary search

--- binary-search.js
+++ binary-search.js
@@ -1,21 +1,28 @@
 /**
  * Recursive binary search
  */
-function binarySearch(array, search) {
+function recursiveBinarySearch(array, search) {
+	let low = 0,
+		high = array.length - 1;
 
-	let low = array[0],
-		high = array[array.length - 1];
-
-	if (low > array[array.length - 1]) {
+	if (low > high) {
 		return false;
+	} else {
+		let middle = Math.floor((low + high) / 2);
+
+		if (search === array[middle]) {
+			return true;
+		} else if (search > array[middle]) {
+			return recursiveBinarySearch(array.splice(middle + 1, array.length - 1), search);
+		} else {
+			return recursiveBinarySearch(array.splice(0, middle), search);
+		}
 	}
-
-
 }
 
-let result = binarySearch(
+let result = recursiveBinarySearch(
 	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-	9
+	12
 );
 
 console.log(result);
